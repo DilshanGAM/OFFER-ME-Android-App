0df71ee8af48273201e9ec749ce05abf0e179019
Dilshan GAM <it21180934@my.sliit.lk>
2023-03-10 15:39:09 +0530

    Removing app bar from splash screen

--- app/src/main/java/com/example/onlinepromotionsexplorer/LoadScreen.kt
+++ app/src/main/java/com/example/onlinepromotionsexplorer/LoadScreen.kt
@@ -7,5 +7,6 @@ class LoadScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load_screen)
+        supportActionBar?.hide();
     }
 }

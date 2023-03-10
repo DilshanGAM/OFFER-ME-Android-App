f5707f459026248fba8232470e19aca895aebdf2
Dilshan GAM <it21180934@my.sliit.lk>
2023-03-10 15:17:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/onlinepromotionsexplorer/LoadScreen.kt
@@ -0,0 +1,11 @@
+package com.example.onlinepromotionsexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_screen)
+    }
+}

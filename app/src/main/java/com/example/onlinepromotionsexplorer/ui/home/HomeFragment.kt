4f56b5c0229adbb6c5d2e348a87b501357de342c
Dilshan GAM <it21180934@my.sliit.lk>
2023-03-18 01:04:13 +0530

    Finished Homepage Function UI

--- app/src/main/java/com/example/onlinepromotionsexplorer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/onlinepromotionsexplorer/ui/home/HomeFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.onlinepromotionsexplorer.R
 import com.example.onlinepromotionsexplorer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,16 +24,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.root.findViewById(R.id.toolbar))
         return root
     }
 

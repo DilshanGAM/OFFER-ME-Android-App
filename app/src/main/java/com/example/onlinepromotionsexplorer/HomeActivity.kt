a7e8b504b132516019239f1b8604ee17f408f700
Nipuna Nadeeshan <89896805+it21166556@users.noreply.github.com>
2023-03-18 02:06:32 +0530

    Revert "Pr test"

--- app/src/main/java/com/example/onlinepromotionsexplorer/HomeActivity.kt
+++ app/src/main/java/com/example/onlinepromotionsexplorer/HomeActivity.kt
@@ -1,7 +1,6 @@
 package com.example.onlinepromotionsexplorer
 
 import android.os.Bundle
-import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -26,7 +25,14 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_account,R.id.navigation_bookmarks
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        supportActionBar?.hide()
         UIEffects.setValidStatusBar(this)
     }
 }

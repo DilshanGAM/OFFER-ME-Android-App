ed9ed40178b65d0219d05927e5e827c15f540427
IT21180552 <it21180552@my.sliit.lk>
2023-05-01 19:22:17 +0530

    manage Offer IT21180552

---
+++ app/src/main/java/com/example/onlinepromotionsexplorer/OfferListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onlinepromotionsexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OfferListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offer_list)
+    }
+}
---
+++ app/src/main/java/com/example/onlinepromotionsexplorer/ViewOfferActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onlinepromotionsexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewOfferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_offer)
+    }
+}

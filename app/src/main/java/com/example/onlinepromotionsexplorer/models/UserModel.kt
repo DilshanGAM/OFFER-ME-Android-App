0cfb7e5b8c0e63b0fbae8d458a049bd87c6f3e9a
Shasthri Kawya <it21165016@my.sliit.lk>
2023-05-15 20:37:05 +0530

    commit to profile form ui and another updates Notification button error cannot be fixed

--- app/src/main/java/com/example/onlinepromotionsexplorer/models/UserModel.kt
+++ app/src/main/java/com/example/onlinepromotionsexplorer/models/UserModel.kt
@@ -1,6 +1,6 @@
 package com.example.onlinepromotionsexplorer.models
 
-data class UserModel(var documentId : String , var name : String ,  var phone : String ,  var profileImage : String)
+data class UserModel(var documentId : String , var name : String ,  var phone : String ,  var profileImage : String, var location : String)
 {
-    constructor() : this("","","","")
+    constructor() : this("","","","","")
 }
